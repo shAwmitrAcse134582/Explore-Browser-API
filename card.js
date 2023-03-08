6466d40df4f8e8ef3808e087bad78fc6a9366f12
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2023-03-08 11:57:41 +0600

    shopping cart

--- card.js
+++ card.js
@@ -8,11 +8,30 @@ const getInputValue = id => {
 const addProduct = () => {
     const product = getInputValue('product-name-field');
     const quantity = getInputValue('product-quantity-field');
-    displayProducts(product, quantity)
+    displayProducts(product, quantity);
     console.log(product, quantity);
 
     //*set to local Storage
-    localStorage.setItem(product, quantity);
+    // localStorage.setItem(product, quantity);
+    saveItemToLocalStorage(product, quantity)
+
+}
+const getshoppingCartFromLocalStorage = () => {
+    let savedCart = localStorage.getItem('cart')
+    let cart = {};
+    if (savedCart) {
+        cart = JSON.parse(savedCart);
+    }
+    return cart;
+}
+
+const saveItemToLocalStorage = (product, quantity) => {
+    const cart = getshoppingCartFromLocalStorage();
+    //*add product to the object as property
+    cart[product] = quantity;
+    const cartStringified = JSON.stringify(cart);
+    //*saved to local storage
+    localStorage.setItem('cart', cartStringified);
 
 }
 
@@ -22,3 +41,14 @@ const displayProducts = (product, quantity) => {
     li.innerText = `${product}:${quantity}`;
     productContainer.appendChild(li);
 }
+
+
+const displayStoredCarts = () => {
+    const cart = getshoppingCartFromLocalStorage();
+    for (const product in cart) {
+        const quantity = cart[product];
+        console.log(product, quantity);
+    }
+}
+
+displayStoredCarts();
