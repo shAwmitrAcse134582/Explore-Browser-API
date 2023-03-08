f42e40220342e93b21b49551229879cff1f8e65e
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2023-03-08 22:29:35 +0600

    confirm practise

---
+++ confirm.js
@@ -0,0 +1,9 @@
+const ask = () => {
+    const ans = confirm('Do you want to show my browser');
+    if (ans == true) {
+        alert('ok! I will say you')
+    }
+    else {
+        alert('ok fine!You have no need to see it');
+    }
+}
